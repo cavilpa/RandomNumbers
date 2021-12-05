25048910255b268a192ddae70268e501a138e958
cavilpa <carlosavilpa@gmail.com>
2021-12-05 10:45:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,16 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>dado</title>
-    <style>
-        table {
-            font-family: arial, sans-serif;
-            border-collapse: collapse;
-            width: 100%;
-            text-align: center;
-            width: 50%;
-            border: 1px solid;
-        }
-    </style>
+    
 </head>
 <body>
 
