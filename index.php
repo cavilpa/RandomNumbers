c49fd59cc4508fe56f19ec378f5e4349f0ae640a
cavilpa <carlosavilpa@gmail.com>
2021-12-05 10:35:10 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dado</title>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+            text-align: center;
+            width: 50%;
+            border: 1px solid;
+        }
+    </style>
+</head>
+<body>
+
+    <form action="process.php" method="post">
+        <label for="min">Inserta un numero >0 </label><br>
+        <input type="number" id="min" name="min" placeholders="Inserta un numero >0" /> <br> <br>
+        <label for="max">Inserta un numero <=12 </label><br>
+        <input type="number" id="max" name="max" placeholders="Inserta un numero <=12"/>
+        <input type="submit" value="Enviar"/>
+    </form>
+
+</body>
+</html>
